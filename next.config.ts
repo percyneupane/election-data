5d3c68534ff2617ba5ad85b25acccd5ee5320044
Prasiddha Neupane <149570068+percyneupane@users.noreply.github.com>
2026-03-10 18:29:52 -0400

    new dev

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ["192.168.7.239"],
+  experimental: {
+    allowedDevOrigins: ["192.168.7.239"],
+  },
 };
 
 export default nextConfig;
